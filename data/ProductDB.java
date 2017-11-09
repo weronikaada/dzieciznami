db0c6b84b95186a9ca89ad4f316fb8fb418d08a6
Majuska93 <33498002+Majuska93@users.noreply.github.com>
2017-11-09 15:15:34 +0100

    Update ProductDB.java

--- data/ProductDB.java
+++ data/ProductDB.java
@@ -1,5 +1,30 @@
 package data;
 
-public class ProductDB {
+import view.*;
+import control.*;
+import model.*;
+import java.util.*;
 
+public class ProductDB {
+private ArrayList<Product> productList = new ArrayList<Product>();
+//constructor
+public ProductDB() {
+}
+
+public Product browseByProductID(String productID) {
+	for(int i=0;i<productList.size();i++) {
+		if(productID.equals(productList.get(i).getProductID())) {
+			return productList.get(i);
+		}
+	}
+	return new Product();
+}
+public void assignProductList() throws Exception{
+	Scanner input = new Scanner(new java.io.File("productDB.txt"));
+	while(input.hasNextLine()) {
+		Product product = new Product(input.next(), input.next(), input.next(),input.nextDouble(),input.next(),input.nextInt());
+		productList.add(product);
+	}
+	input.close();
+}
 }
